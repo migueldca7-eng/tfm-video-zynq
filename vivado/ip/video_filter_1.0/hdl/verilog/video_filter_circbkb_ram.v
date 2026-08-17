f98fd753801c5e3f58d65b2d958c362ce36d0328
migueldca7-eng <252050341+migueldca7-eng@users.noreply.github.com>
2026-08-17 21:10:49 +0200

    build(hls): package video filter IP

---
+++ vivado/ip/video_filter_1.0/hdl/verilog/video_filter_circbkb_ram.v
@@ -0,0 +1,54 @@
+// ==============================================================
+// Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.1 (64-bit)
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+`timescale 1 ns / 1 ps
+module video_filter_circbkb_ram (addr0, ce0, d0, we0, q0, addr1, ce1, d1, we1,  clk);
+
+parameter DWIDTH = 34;
+parameter AWIDTH = 10;
+parameter MEM_SIZE = 641;
+
+input[AWIDTH-1:0] addr0;
+input ce0;
+input[DWIDTH-1:0] d0;
+input we0;
+output reg[DWIDTH-1:0] q0;
+input[AWIDTH-1:0] addr1;
+input ce1;
+input[DWIDTH-1:0] d1;
+input we1;
+input clk;
+
+(* ram_style = "block" *)reg [DWIDTH-1:0] ram[0:MEM_SIZE-1];
+
+
+
+
+always @(posedge clk)  
+begin 
+    if (ce0) 
+    begin
+        if (we0) 
+        begin 
+            ram[addr0] <= d0; 
+        end 
+        q0 <= ram[addr0];
+    end
+end
+
+
+always @(posedge clk)  
+begin 
+    if (ce1) 
+    begin
+        if (we1) 
+        begin 
+            ram[addr1] <= d1; 
+        end 
+    end
+end
+
+
+endmodule
+
